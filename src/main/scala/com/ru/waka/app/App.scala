d21d2fa96eeeb361ede28e811ea5bb10d69f2848
Arata Tanaka <tarata43@yahoo.co.jp>
2017-06-01 23:10:16 +0900

    Add json

--- src/main/scala/com/ru/waka/app/App.scala
+++ src/main/scala/com/ru/waka/app/App.scala
@@ -1,8 +1,21 @@
 package com.ru.waka.app
 
-class App extends AppStack {
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.json._
+
+class App extends AppStack with JacksonJsonSupport {
+  protected implicit val jsonFormats: Formats = DefaultFormats.withBigDecimal
 
   get("/") {
     com.ru.waka.html.index.render()
   }
+
+  get("/json") {
+    contentType = formats("json")
+    App.Hoge("aaa")
+  }
+}
+
+object App {
+  case class Hoge(a: String)
 }
